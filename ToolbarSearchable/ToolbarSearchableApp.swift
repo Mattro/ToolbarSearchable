031e4ab49345e52d923d97cdf46ef3c15a13914e
mattro <matt@mattro.design>
2025-12-01 10:46:05 +1030

    Initial Commit

---
+++ ToolbarSearchable/ToolbarSearchableApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToolbarSearchableApp.swift
+//  ToolbarSearchable
+//
+//  Created by Matt on 1/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct ToolbarSearchableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
